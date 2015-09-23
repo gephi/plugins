31d4c37e6406d4016c028d2438f01b493a09ff60
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2015-09-23 17:45:01 +0200

    Integrate angucomplete (search autocompletion)

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('gephiPluginsFront', [
   'ngRoute'
+, 'angucomplete-alt'
 , 'gephiPluginsFront.services'
 , 'gephiPluginsFront.directives'
 , 'gephiPluginsFront.home'
